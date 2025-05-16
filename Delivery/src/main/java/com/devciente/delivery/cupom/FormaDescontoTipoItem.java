ed5b1bc244827ce304bc4e3464d92d53bc969171
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-05-15 21:00:33 -0300

    feat: add formaDescontoTipoItem

--- Delivery/src/main/java/com/devciente/delivery/cupom/FormaDescontoTipoItem.java
+++ Delivery/src/main/java/com/devciente/delivery/cupom/FormaDescontoTipoItem.java
@@ -1,26 +1,37 @@
 package com.devciente.delivery.cupom;
 
+import com.devciente.delivery.Item;
 import com.devciente.delivery.Pedido;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FormaDescontoTipoItem implements FormaDescontoTaxaEntregaInterface{
-    Map<String, Double> descontosPorTipoItem = new HashMap<String, Double>();
-    
+public class FormaDescontoTipoItem implements FormaDescontoTaxaEntregaInterface {
 
-    public FormaDescontoTipoItem(){
-        descontosPorTipoItem.put("Alimentos", 2.00);
+    Map<String, Double> descontosPorTipoItem = new HashMap<String, Double>();
+
+    public FormaDescontoTipoItem() {
+        descontosPorTipoItem.put("Alimentação", 5.00);
+        descontosPorTipoItem.put("Educação", 2.00);
+        descontosPorTipoItem.put("Lazer", 1.50);
     }
-    
+
     @Override
     public CupomDescontoEntrega calcularDesconto(Pedido pedido) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        double descontoTotalPorItem = 0;
+
+        for (Item item : pedido.getItens()) {
+            String tipo = item.getTipo();
+            if (descontosPorTipoItem.containsKey(tipo)) {
+                descontoTotalPorItem += descontosPorTipoItem.get(tipo);
+            }
+        }
+        return new CupomDescontoEntrega("Desconto por tipo de item", descontoTotalPorItem);
     }
 
     @Override
     public boolean seAplica(Pedido pedido) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return pedido.getItens().stream().anyMatch(item
+                -> descontosPorTipoItem.containsKey(item.getTipo())
+        );
     }
-    
-    
 }
