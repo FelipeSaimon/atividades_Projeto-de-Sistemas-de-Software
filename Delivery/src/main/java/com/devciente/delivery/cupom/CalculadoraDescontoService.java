d81efb7143e2938ac70af1c355e441f3ad116799
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-05-18 21:41:38 -0300

    feat: aplicando o calculo de descontos as formas de descontos

--- Delivery/src/main/java/com/devciente/delivery/cupom/CalculadoraDescontoService.java
+++ Delivery/src/main/java/com/devciente/delivery/cupom/CalculadoraDescontoService.java
@@ -1,12 +1,30 @@
 package com.devciente.delivery.cupom;
 
 import com.devciente.delivery.Pedido;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculadoraDescontoService {
-    public List<CupomDescontoEntrega> calcularDesconto(Pedido pedido){
-        
-        return null;
+
+    List<FormaDescontoTaxaEntregaInterface> formasDesconto = new ArrayList<>();
+
+    public CalculadoraDescontoService(){
+        formasDesconto.add(new FormaDescontoPorBairro());
+        formasDesconto.add(new FormaDescontoTipoCliente());
+        formasDesconto.add(new FormaDescontoTipoItem());
     }
-    
+
+    public List<CupomDescontoEntrega> calcularDesconto(Pedido pedido){
+        List<CupomDescontoEntrega> cupons = new ArrayList<>();
+
+        for (FormaDescontoTaxaEntregaInterface formaDesconto : formasDesconto) {
+            if (formaDesconto.seAplica(pedido)) {
+                CupomDescontoEntrega cupom = formaDesconto.calcularDesconto(pedido);
+                cupons.add(cupom);
+            }
+        }
+
+        return cupons;
+    }
 }
