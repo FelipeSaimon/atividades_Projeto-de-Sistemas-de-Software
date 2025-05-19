2aeb6f7eff520cc02507906b1b92cf8b55af7d82
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-05-18 21:43:37 -0300

    feat: criando FormaDescontoTipoCliente e FormaDescontoPorBairro

---
+++ Delivery/src/main/java/com/devciente/delivery/cupom/FormaDescontoPorBairro.java
@@ -0,0 +1,47 @@
+package com.devciente.delivery.cupom;
+
+import com.devciente.delivery.Cliente;
+import com.devciente.delivery.Pedido;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormaDescontoPorBairro implements FormaDescontoTaxaEntregaInterface{
+
+	String bairroCliente;
+	Map<String, Double> descontosPorBairro = new HashMap<String, Double>();
+
+	public FormaDescontoPorBairro() {
+		descontosPorBairro.put("Bela Vista", 3.00);
+		descontosPorBairro.put("Centro", 2.00);
+		descontosPorBairro.put("Cidade Maravilhosa", 1.50);
+	}
+
+	@Override
+	public CupomDescontoEntrega calcularDesconto(Pedido pedido) {
+		double descontoTotalPorBairro = 0;
+
+		bairroCliente = pedido.getCliente().getBairro();
+
+		if (bairroCliente != null) {
+			// Verifica se o cliente tem um bairro definido
+			if (descontosPorBairro.containsKey(bairroCliente)) {
+				// e se esse bairro está na lista de descontos, e insere o desconto
+				descontoTotalPorBairro += descontosPorBairro.get(bairroCliente);
+			}
+		}
+
+		return new CupomDescontoEntrega("Desconto por bairro", descontoTotalPorBairro);
+	}
+
+	@Override
+	public boolean seAplica(Pedido pedido) {
+		bairroCliente = pedido.getCliente().getBairro();
+
+		if (bairroCliente != null) {
+			// Verifica se o cliente tem um bairro definido
+			return descontosPorBairro.containsKey(bairroCliente);
+		}
+		return false;
+	}
+}
---
+++ Delivery/src/main/java/com/devciente/delivery/cupom/FormaDescontoTipoCliente.java
@@ -0,0 +1,42 @@
+package com.devciente.delivery.cupom;
+
+import com.devciente.delivery.Pedido;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormaDescontoTipoCliente implements FormaDescontoTaxaEntregaInterface{
+
+	Map<String, Double> descontosPortipoCliente = new HashMap<String, Double>();
+	String tipoCliente;
+
+	public FormaDescontoTipoCliente() {
+		descontosPortipoCliente.put("Ouro", 3.00);
+		descontosPortipoCliente.put("Prata", 2.00);
+		descontosPortipoCliente.put("Bronze", 1.00);
+	}
+
+	@Override
+	public CupomDescontoEntrega calcularDesconto(Pedido pedido) {
+		double descontoTotalPorTipoCliente = 0;
+
+		// Verifica se o cliente tem um tipo definido
+		if (pedido.getCliente().getTipoCliente() != null) {
+			tipoCliente = pedido.getCliente().getTipoCliente();
+			// e se esse tipo está na lista de descontos, e insere o desconto
+			if (descontosPortipoCliente.containsKey(tipoCliente)) {
+				descontoTotalPorTipoCliente += descontosPortipoCliente.get(tipoCliente);
+			}
+		}
+
+		return new CupomDescontoEntrega("Desconto por tipo de cliente", descontoTotalPorTipoCliente);
+	}
+
+	@Override
+	public boolean seAplica(Pedido pedido) {
+		if (pedido.getCliente().getTipoCliente() != null) {
+			return descontosPortipoCliente.containsKey(pedido.getCliente().getTipoCliente());
+		}
+		return false;
+	}
+}
