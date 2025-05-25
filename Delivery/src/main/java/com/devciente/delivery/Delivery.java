88e510923999dcc35954f5cea74e0284d6870bba
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-05-25 11:03:33 -0300

    ajustando branchs

--- Delivery/src/main/java/com/devciente/delivery/Delivery.java
+++ Delivery/src/main/java/com/devciente/delivery/Delivery.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
 package com.devciente.delivery;
 
 import com.devciente.delivery.cupom.CalculadoraDescontoService;
@@ -17,17 +18,17 @@ public class Delivery {
 //        System.out.println("Hello World!");
 
         Pedido pedido = new Pedido(new Cliente("Saimon", "Ouro", "Bela Vista"), LocalDate.now());
-        
+
         pedido.adicionarItem(new Item("Feijoada", 1, 50.24, "Alimentacao"));
         pedido.adicionarItem(new Item("Caderno", 3, 10.99, "Educacao"));
 
         CalculadoraDescontoService calculadora = new CalculadoraDescontoService();
-        
+
         calculadora.calcularDesconto(pedido);
-        
+
         System.out.println("Itens: " + pedido.getCuponsDescontoEntrega());
-        
+
         System.out.println("taxa atual " + pedido.getTaxaEntrega());
-      
+
     }
 }
